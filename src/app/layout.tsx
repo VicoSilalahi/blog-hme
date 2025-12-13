c4a8110d04c0eab4b6a738592aeea8ecfa7fedf3
Vico A.C. Silalahi <164010802+silalahivico@users.noreply.github.com>
2025-12-14 00:18:29 +0700

    Updated React for Vulnerability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Blog HME Jimmyahhh",
   description: "Jimmyahhh blog bebas aja sih",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
